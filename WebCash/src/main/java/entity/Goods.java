9f779bf83bcc79b585cc2916abb46f78452121b0
wqq3670 <1121093520@qq.com>
2020-09-06 23:22:38 +0800

    web收银台系统

---
+++ WebCash/src/main/java/entity/Goods.java
@@ -0,0 +1,28 @@
+package entity;
+
+import lombok.Data;
+
+
+@Data
+public class Goods {
+    private Integer id;
+    private String name;
+    private String introduce;
+    private Integer stock;
+    private String unit;
+    private Integer price;//商品价格  12.34  -》 1234
+    private Integer discount;
+
+    private Integer buyGoodsNum;
+
+    public double getPrice() {
+        return price * 1.0 / 100;
+    }
+    //这个是返回整数的价格
+    public int getPriceInt() {
+        return price;
+    }
+
+
+
+}

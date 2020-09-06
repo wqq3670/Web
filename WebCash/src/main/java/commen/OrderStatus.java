5a25d5fb3a49d0f4dcc37c92dd66929b479f330f
wqq3670 <1121093520@qq.com>
2020-09-06 23:57:22 +0800

    web收银台

---
+++ WebCash/src/main/java/commen/OrderStatus.java
@@ -0,0 +1,30 @@
+package commen;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@ToString
+
+public enum OrderStatus {
+    PLAYING(1,"待支付"),OK(2,"支付完成");
+    private int flg;//标记
+    private String desc;//描述
+
+    OrderStatus(int flg,String desc) {
+        this.flg = flg;
+        this.desc = desc;
+    }
+
+    public static OrderStatus valueOf(int flg) {
+        for (OrderStatus orderStatus : values()) {
+            if(orderStatus.flg == flg) {
+                return orderStatus;
+            }
+        }
+        throw new RuntimeException("orderStatus flg"
+                + flg + "not fount!");
+    }
+}
